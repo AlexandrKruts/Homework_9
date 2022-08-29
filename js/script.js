746dcce283742d148516c9cc75820207ae5bcd2f
AlexandrKruts <kruts704@gmail.com>
2022-08-29 21:19:45 +0300

    Done first task

---
+++ js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//1
+
+// let date = new Date("September 04, 2003");
+// let year = date.getFullYear();
+// let month = date.getMonth() + 1;
+// let weekday = date.getDay();
+// console.log(`${weekday}.${month}.${year}`);
